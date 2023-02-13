8a0553acc9bebf3a0844d9958bbd6296a16f4f23
gemario <gmario.sh@gmail.com>
2023-02-13 02:01:53 -0300

    aplicação finalizada

---
+++ src/pages/Status.tsx
@@ -0,0 +1,66 @@
+import { PaperPlaneRight } from "phosphor-react";
+import { FormEvent, KeyboardEvent, useState } from "react";
+import { Header } from "../components/Header"
+import { Separator } from "../components/Separator"
+import { Tweet } from "../components/Tweet"
+
+import './Status.css';
+
+export function Status() {
+  const [newAnswer, setNewAnswer] = useState('');
+  const [answers, setAnswers] = useState([
+    'Concordo...',
+    'Olha, faz sentido!',
+    'Parabéns pelo progresso.'
+  ])
+
+  function createNewAnswer(event: FormEvent) {
+    event.preventDefault()
+
+    setAnswers([newAnswer, ...answers ])
+    setNewAnswer('')
+  }
+
+  function handleHotKeySubmit(event: KeyboardEvent) {
+    if (event.key === 'Enter' && (event.ctrlKey || event.metaKey)) {
+      setAnswers([newAnswer, ...answers ])
+      setNewAnswer('')
+    }
+  }
+
+
+  return (
+    <main className="status">
+      <Header title="Tweet"/>
+
+      <Tweet content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, ducimus sit. Laborum nulla dolores cum nisi explicabo modi, non consequatur mollitia molestias perspiciatis officiis culpa nobis eveniet pariatur. Maiores, rem."/>
+
+      <Separator />
+
+      <form onSubmit={createNewAnswer} className="answer-tweet-form">
+        <label htmlFor="tweet">
+          <img src="https://github.com/gemario.png" alt="Jorge Mario" />
+          <textarea 
+            id="tweet" 
+            placeholder="Tweet your answer"
+            value={newAnswer}
+            onKeyDown={handleHotKeySubmit}
+            onChange={(event) => {
+              setNewAnswer(event.target.value)
+            }}
+          />
+        </label>
+
+        <button type="submit">
+          <PaperPlaneRight />
+          <span>Answer</span>
+        </button>
+      </form>
+
+
+      {answers.map(answer => {
+        return <Tweet key={answer} content={answer} />
+      })}
+    </main>
+  )
+}
